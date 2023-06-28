8dbdb4d9a700512a3fc98dedc067717ad93f0602
erikhalt <erikhalt@hotmail.com>
2023-06-29 00:12:37 +0200

    moved files to root

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
