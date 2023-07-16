ac12d1d50182d92920a28d118fcc53ee0c02d613
erikhalt <erikhalt@hotmail.com>
2023-07-17 00:27:46 +0200

    started dijkstras

--- static/js/pathfinding_array.js
+++ static/js/pathfinding_array.js
@@ -48,9 +48,124 @@ function choosen_algorithm() {
     if (algo == " ") {
         return
     }
-    else if (algo != " ") {
-        console.log(algo)
+    else if (algo == "djikstra") {
+        djikstra_algo()
     }
+    else if (algo == "astar") {
+        astar_algo()
+    }
+}
+function astar_algo() {
+
+}
+
+var dijkstra_array;
+
+function djikstra_algo() {
+    solution_found = false;
+    class Node {
+        constructor(i, j, element) {
+            this.G;
+            this.i = i;
+            this.j = j;
+            this.element = element;
+            this.start = false;
+            this.end = false;
+            this.obstacle = false;
+
+            if (this.element.style.backgroundColor == 'green') {
+                this.start = true;
+            };
+
+            if (this.element.style.backgroundColor == 'red') {
+                this.end = true;
+            };
+
+            if (this.element.style.backgroundColor == 'grey') {
+                this.obstacle = true;
+            };
+        }
+    }
+
+
+    dijkstra_array = []
+
+    for (i = 0; i < columns; i++) {
+        list = []
+        for (j = 0; j < columns; j++) {
+            list.push(new Node(i, j, array[i][j]));
+        }
+        dijkstra_array.push(list)
+    }
+    openSet = []
+    closedSet = []
+
+    for (const m of dijkstra_array) {
+        for (const n of m) {
+            if (n.start == true) {
+                start_i = n.i;
+                start_j = n.j;
+            }
+            else if (n.end == true) {
+                end_i = n.i;
+                end_j = n.j;
+            }
+        }
+    }
+
+    function draw_openset() {
+        if (openSet.length == 0) {
+            return
+        }
+        for (const c of openSet) {
+            console.log('draw')
+            c.element.style.backgroundColor = 'lightblue'
+        }
+    }
+
+    function draw_closedset() {
+
+        if (closedSet.length == 0) {
+            return
+        }
+        for (const c of closedSet) {
+            c.element.style.backgroundColor = 'blue'
+        }
+    }
+    // Start the algorithm
+
+    if (start_i < columns - 1) {
+        openSet.push(dijkstra_array[start_i + 1][start_j])
+        dijkstra_array[start_i + 1][start_j].G = 10
+    }
+    if (start_i > 0) {
+        openSet.push(dijkstra_array[start_i - 1][start_j])
+        dijkstra_array[start_i - 1][start_j].G = 10
+    }
+
+    if (start_j < columns - 1) {
+        openSet.push(dijkstra_array[start_i][start_j + 1])
+        dijkstra_array[start_i][start_j + 1].G = 10
+    }
+    if (start_j > 0) {
+        openSet.push(dijkstra_array[start_i][start_j - 1])
+        dijkstra_array[start_i][start_j - 1].G = 10
+    }
+
+    while (solution_found != true) {
+        for (i = 0; i < openSet.length; i++) {
+            if (openSet[i].obstacle == true) {
+                openSet.splice(i, 1);
+            }
+        }
+        draw_openset();
+        draw_closedset();
+        solution_found = true;
+
+    }
+
+
+
 }
 
 
